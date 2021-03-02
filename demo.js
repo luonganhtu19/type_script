29220eb767ad0c4a33f6cb6cb8cf9c49c7c4cb18
- <->
2021-03-02 23:22:40 +0700

    vrs1

---
+++ demo.js
@@ -0,0 +1,39 @@
+/**
+ * if/else
+ * */
+var count = 50;
+if (count > 0) {
+    count--;
+}
+else {
+    count = 0;
+}
+console.log(count);
+/**
+ * loop
+ * */
+console.log("for loop");
+var keys = 'a1a1a1a1';
+for (var idx_1 = 0; idx_1 < keys.length; ++idx_1) {
+    console.log(keys[idx_1]);
+}
+console.log("while loop");
+var idx = 0;
+while (idx < keys.length) {
+    console.log(keys[idx]);
+    ++idx;
+}
+console.log("do-while loop");
+idx = 0;
+do {
+    console.log(keys[idx]);
+    ++idx;
+} while (idx < keys.length);
+console.log("for-of loop");
+var user = {
+    name: 'Bob',
+    age: 55
+};
+for (var key in user) {
+    console.log(key + ": " + user[key]);
+}
---
+++ demo.ts
@@ -0,0 +1,42 @@
+/**
+ * if/else
+ * */
+let count=50;
+if (count>0){
+    count--;
+}else {
+    count=0;
+}
+console.log(count);
+
+/**
+ * loop
+ * */
+console.log("for loop");
+
+const keys='a1a1a1a1';
+for (let idx=0;idx<keys.length;++idx){
+    console.log(keys[idx]);
+}
+console.log("while loop");
+let idx=0;
+while (idx<keys.length){
+    console.log(keys[idx]);
+    ++idx;
+}
+
+console.log("do-while loop")
+idx=0;
+do {
+    console.log(keys[idx]);
+    ++idx;
+}while (idx<keys.length);
+
+console.log("for-of loop");
+const user={
+    name:'Bob',
+    age:55
+};
+for (const key in user){
+    console.log(`${key}: ${user[key]}`);
+}
